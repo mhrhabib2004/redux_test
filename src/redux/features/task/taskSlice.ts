9f59c579d8002abf8e41844de63c5ac4484a48f6
mhrhabib2004 <mhrhabib2004@gmail.com>
2025-01-09 02:17:32 +0600

    add:types

--- src/redux/features/task/taskSlice.ts
+++ src/redux/features/task/taskSlice.ts
@@ -1,14 +1,21 @@
+import { ITask } from "@/types";
 import { createSlice} from "@reduxjs/toolkit";
 
-const initialState = {
-    task:{
-        id:"asdf",
-        title:"Initialize frontend",
-        description:"create Home page,and routing",
-        dueDate:2025-11,
-        isCompleted:false,
-        priority: "High",
-    }
+interface IinitialState{
+    task:ITask[];
+}
+const initialState : IinitialState = {
+    task:[
+        {
+            id:"asdf",
+            title:"Initialize frontend",
+            description:"create Home page,and routing",
+            dueDate:"2025-01-09",
+            isCompleted:false,
+            priority: "High",
+        }
+    
+    ],
 }
 const taskSlice = createSlice({
     name:"task",
