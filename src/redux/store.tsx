fcef3a0f76d9fa3414668c3e5e0fe924d4c00f80
mhrhabib2004 <mhrhabib2004@gmail.com>
2025-01-08 22:44:19 +0600

    up:store

--- src/redux/store.tsx
+++ src/redux/store.tsx
@@ -1,13 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import logger from "./middlewares/logger";
-import counterSlice from "./features/counter/counterSlice"
-
+import counterReducer from "./features/counter/counterSlice"
+import taskReducer from "./../redux/features/task/taskSlice"
 
 
 export const store = configureStore({
     reducer: {
-        counter: counterSlice,
-        // todo: taskReducer,
+        counter: counterReducer,
+        task: taskReducer,
         // user: userReducer
     },
     middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger)
